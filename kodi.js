0c1fe45068f0af5a9d2f3c7c144d796ae0c3e3c0
5AMsan <s.petat@sporting-comm.fr>
2019-02-22 18:05:01 +0000

    added basic movie & tv shows search & play feature. Closes #5

--- kodi.js
+++ kodi.js
@@ -1,7 +1,6 @@
 var request = require('request-promise-native');
 
 var AssistantKodi = function(configuration) {
-
   if (configuration.hosts) {
     this.hosts = configuration.hosts
   // legacy conf
@@ -13,6 +12,8 @@ var AssistantKodi = function(configuration) {
     this.connectionTimeout = configuration.connectionTimeout | 10000
     this.sendTimeout = configuration.sendTimeout | 3000
   }
+
+  this.hostConf = null
 
 }
 
@@ -34,7 +35,7 @@ AssistantKodi.prototype.init = function(plugins) {
  * @param {Object} [params] Peut contenir 'value1', 'value2' et 'value3'
  *
  */
-AssistantKodi.prototype.action = function(commande) {
+AssistantKodi.prototype.action = function(commande, params) {
   var method = commande,
       params = null,
       hostConf = null
@@ -61,7 +62,8 @@ AssistantKodi.prototype.action = function(commande) {
       return false;
     }
 
-  } else {
+  }
+  else {
     if ( this.hosts[Object.keys(this.hosts)[0]] )
       hostConf = this.hosts[Object.keys(this.hosts)[0]].host + ":" + this.hosts[Object.keys(this.hosts)[0]].port
     // legacy conf
@@ -69,29 +71,105 @@ AssistantKodi.prototype.action = function(commande) {
       hostConf = this.host + ":" + this.port
   }
 
-  // si '#' dans la commande, on recupère l'objet
-  if ( method.indexOf('#') > -1) {
-    var arrParams = method.split('#')
-    method = arrParams[0]
-    params = JSON.parse(arrParams[1])
-    var body = {
-      jsonrpc: "2.0",
-      id: 1,
-      method: method,
-      params: params
+  AssistantKodi.hostConf = hostConf
+
+  // Manage search&play feature
+  if ( method.indexOf('sandp') === -1 ) {
+    // si '#' dans la commande, on recupère l'objet
+    if ( method.indexOf('#') > -1) {
+      var arrParams = method.split('#')
+      method = arrParams[0]
+      params = JSON.parse(arrParams[1])
+      var body = {
+        jsonrpc: "2.0",
+        id: 1,
+        method: method,
+        params: params
+      }
+
+    } else {
+      var body = {
+        jsonrpc: "2.0",
+        id: 1,
+        method: method
+      }
+
     }
 
-  } else {
-    var body = {
-      jsonrpc: "2.0",
-      id: 1,
-      method: method
+    var req = {
+      url: 'http://' + hostConf + "/jsonrpc",
+      headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': 'assistant-plugin'
+      },
+      method: 'POST',
+      body: body,
+      json: true
     }
+
+    request(req)
+    .then(function(response){
+      //console.log(response.result)
+      return (response)
+    })
+    .catch(function(err){
+      console.log(err)
+      return (err)
+    })
 
   }
 
+  // Ths is a search request
+  else {
+    if ( method.indexOf('#') === -1 ) {
+      console.log("no parameters found for search")
+      return false
+    }
+
+    var arrParams = method.split('#')
+    if ( arrParams.length < 3) {
+      console.log("missing parameters for search : sandp#Title#tvshow")
+      return false
+    }
+
+     // method = arrParams[0] // always sandp
+    var title = arrParams[1]
+    var library = arrParams[2]
+    var inProgress = Boolean(arrParams[3])
+    AssistantKodi.search ( title, library, inProgress )
+  }
+
+};
+
+AssistantKodi.search = (title, library, inProgress) => {
+
+  switch (library) {
+    case 'movie' :
+      var method = "VideoLibrary.GetMovies"
+      var id = "libMovies"
+      var filter = { operator: "contains", field: "title", value: title }
+      break
+    default :
+      var method = inProgress ? "VideoLibrary.GetInProgressTVShows" :"VideoLibrary.GetTVShows"
+      var id = "libTvShows"
+      var filter = { and: [ {operator: "contains", field: "title", value: title }, {field: "playcount", operator: "is", value: "0"} ] }
+      break
+  }
+
+  var body = {
+    jsonrpc: "2.0",
+    id: id,
+    method: method,
+    params: {
+      sort: { order: "ascending", method: "title", ignorearticle: true },
+      filter: filter,
+      properties: [ "title", "file" ],
+      limits: { start : 0, end: 5 }
+    }
+  }
+
   var req = {
-    url: 'http://' + hostConf + "/jsonrpc",
+    url: 'http://' + AssistantKodi.hostConf + "/jsonrpc",
     headers: {
         'Content-Type': 'application/json',
         'User-Agent': 'assistant-plugin'
@@ -103,15 +181,118 @@ AssistantKodi.prototype.action = function(commande) {
 
   request(req)
   .then(function(response){
-    //console.log(response.result)
+    AssistantKodi.getSearchItem (response, library)
+    .then(function(response){
+      AssistantKodi.playFromSearch (response.file, response.title)
+    })
+    .catch(function(err){
+      console.log(err)
+      return (err)
+    })
+  })
+  .catch(function(err){
+    console.log(err)
+    return (err)
+  })
+}
+
+AssistantKodi.playFromSearch = (file, title) => {
+  var body = {
+    jsonrpc: "2.0",
+    id: 1,
+    method: "Player.Open",
+    params: {
+      item: { file: file }
+    }
+  }
+  var req = {
+    url: 'http://' + AssistantKodi.hostConf + "/jsonrpc",
+    headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'assistant-plugin'
+    },
+    method: 'POST',
+    body: body,
+    json: true
+  }
+
+  request(req)
+  .then(function(response){
+    console.log("playing "+title)
     return (response)
   })
   .catch(function(err){
     console.log(err)
     return (err)
   })
+}
 
-};
+AssistantKodi.getSearchItem = (response, library) => {
+  return new Promise(function(resolve, reject) {
+
+    switch (library) {
+      case 'movie' :
+        var item = response.result.movies[0]
+        var bestMatch = item.file
+        var bestMatchTitle = item.title
+        break
+      default :
+        var item = response.result.tvshows[0]
+        break
+
+    }
+
+    if (library == 'tvshow') {
+      AssistantKodi.getEpisodes(item.tvshowid)
+      .then(function(response){
+        resolve (response)
+      })
+      .catch(function(err) {
+        console.log(err)
+        reject (err)
+      })
+    } else {
+      resolve({file: bestMatch, title: bestMatchTitle})
+    }
+
+  })
+}
+
+AssistantKodi.getEpisodes = (tvShowId) => {
+  return new Promise(function(resolve, reject) {
+    var body = {
+      jsonrpc: "2.0",
+      id: "libTvShows",
+      method: "VideoLibrary.GetEpisodes",
+      params: {
+        tvshowid: tvShowId,
+        sort: { order: "ascending", method: "playcount" },
+        properties: ['title', 'file'],
+        filter: { field: "playcount", operator: "is", value: "0" }
+      }
+    }
+    var req = {
+      url: 'http://' + AssistantKodi.hostConf + "/jsonrpc",
+      headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': 'assistant-plugin'
+      },
+      method: 'POST',
+      body: body,
+      json: true
+    }
+
+    request(req)
+    .then(function(response){
+      resolve ({file:response.result.episodes[0].file, title:response.result.episodes[0].title} )
+    })
+    .catch(function(err){
+      console.log(err)
+      reject (err)
+    })
+  })
+
+}
 
 /**
  * Initialisation du plugin
